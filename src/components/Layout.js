a608097afe1fbac22c65a8aaede1bf899e34c454
Michelle Tanzil <mtanzil01@gmail.com>
2020-11-02 12:23:38 -0800

    Added react helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
+import { Helmet } from "react-helmet"
+
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false)
 
@@ -11,6 +13,19 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet
+        title="Michelle Tanzil"
+        meta={[
+          { name: "description", content: "Michelle Tanzil's Portfolio" },
+          {
+            name: "keywords",
+            content:
+              "software, developer, dev, full stack, builder, problem solver, Michelle, Tanzil",
+          },
+        ]}
+      >
+        <html lang="en" />
+      </Helmet>
       <Navbar toggleSidebar={toggleSidebar} />
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
