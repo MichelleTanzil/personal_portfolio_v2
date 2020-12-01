7461e85993cef8b56ac2c56647e2cf34aa87684d
Michelle Tanzil <mtanzil01@gmail.com>
2020-11-30 19:30:45 -0800

    Started on the Hero component

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -32,12 +32,25 @@ const Hero = () => {
             <div className="underline"> </div>
             <h1>i'm michelle</h1>
             <Titles />
-            <Link to="/about" className="btn">
+            <Link to="/about" className="brk-btn">
               About Me
             </Link>
           </div>
         </article>
         <Image fluid={fluid} className="hero-img" alt={"Michelle Tanzil"} />
+      </div>
+      <div class="custom-shape-divider-bottom-1606793282">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            class="shape-fill"
+          ></path>
+        </svg>
       </div>
     </header>
   )
