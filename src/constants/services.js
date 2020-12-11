53e40174d03ad46c5c6e97dd2462c83b29a2029c
Michelle Tanzil <mtanzil01@gmail.com>
2020-12-10 16:54:28 -0800

    Some services text

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,22 +1,22 @@
 import React from "react"
-import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+import { FaCode, FaSketch, FaUserFriends } from "react-icons/fa"
 export default [
   {
     id: 1,
-    icon: <FaCode className="service-icon" />,
-    title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    icon: <FaSketch className="service-icon" />,
+    title: "front end development",
+    text: `Familiar with React, Gatsby, HTML, CSS, and JS. `,
   },
   {
     id: 2,
-    icon: <FaSketch className="service-icon" />,
-    title: "web design",
+    icon: <FaCode className="service-icon" />,
+    title: "back end development",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 3,
-    icon: <FaAndroid className="service-icon" />,
-    title: "app design",
+    icon: <FaUserFriends className="service-icon" />,
+    title: "developer support engineer",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
 ]
