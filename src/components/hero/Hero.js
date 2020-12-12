a3fb93192071c6fa505d9640a61cee1a9f0abff6
Michelle Tanzil <mtanzil01@gmail.com>
2020-12-12 14:36:24 -0800

    Styling on hero component

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -38,7 +38,7 @@ const Hero = () => {
           </div>
         </article>
         <Image fluid={fluid} className="hero-img" alt="Michelle Tanzil" />
-        <div class="custom-shape-divider-bottom-1606793282">
+        <div class="custom-shape-divider-bottom-1607810345">
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -46,8 +46,8 @@ const Hero = () => {
             preserveAspectRatio="none"
           >
             <path
-              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-              className="shape-fill"
+              d="M1200 0L0 0 598.97 114.72 1200 0z"
+              class="shape-fill"
             ></path>
           </svg>
         </div>
