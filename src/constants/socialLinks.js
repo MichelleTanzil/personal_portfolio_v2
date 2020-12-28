b8481c01252d242903785c245313142a0b66bdc0
Michelle Tanzil <mtanzil01@gmail.com>
2020-12-28 12:06:13 -0800

    Social links updated

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,37 +1,31 @@
 import React from "react"
 import {
-  FaFacebookSquare,
+  FaEnvelope,
   FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
   FaTwitterSquare,
+  FaGithub,
 } from "react-icons/fa"
 
 const data = [
   {
     id: 1,
-    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaEnvelope className="social-icon"></FaEnvelope>,
+    url: "mailto:mtanzil01@gmail.com?subject=Reach%20out%20to%20Michelle",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/michelletanzil/",
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
+    url: "https://twitter.com/codes_michelle",
   },
   {
     id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaGithub className="social-icon"></FaGithub>,
+    url: "https://github.com/MichelleTanzil",
   },
 ]
 const links = data.map(link => {
