a610c44a70542ada82b0ca382a5acd541ec9599e
Michelle Tanzil <mtanzil01@gmail.com>
2021-01-02 09:55:11 -0800

    Contact component - styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,20 @@ import React from "react"
 
 const Contact = () => {
   return (
-    <section className="section contact">
+    <section className="contact">
+      <div className="custom-shape-divider-top-1609609605">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z"
+            className="shape-fill"
+          ></path>
+        </svg>
+      </div>
       <article className="section-center contact-form">
         <h3>get in touch</h3>
         <form action="https://formspree.io/f/xleongpz" method="POST">
