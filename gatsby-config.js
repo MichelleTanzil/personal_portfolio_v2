da3ed931085e4bbb5dd02e2699af7278fb83f8ac
Michelle Tanzil <mtanzil01@gmail.com>
2021-01-04 07:13:31 -0800

    Added Gatsby smoothscroll plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-smoothscroll`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
