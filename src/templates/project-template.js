2d0b554e10b5abd791e82b287fa3bd4b2d68a4b7
Michelle Tanzil <mtanzil01@gmail.com>
2021-01-06 19:50:20 -0800

    Set up project template

---
+++ src/templates/project-template.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/Layout"
+const ProjectTemplate = () => {
+  return <Layout></Layout>
+}
+
+export default ProjectTemplate
