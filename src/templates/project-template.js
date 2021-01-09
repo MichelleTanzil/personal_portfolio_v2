372fe48686cd7dd4352dcb5526ac7265d5b4795c
Michelle Tanzil <mtanzil01@gmail.com>
2021-01-09 14:47:59 -0800

    Create Projects pages programmatically

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -2,7 +2,17 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 const ProjectTemplate = () => {
-  return <Layout></Layout>
+  return <Layout>Project template</Layout>
 }
+
+export const query = graphql`
+  query GetSingleProject($slug: String) {
+    project: prismicProjects(uid: { eq: $slug }) {
+      data {
+        status
+      }
+    }
+  }
+`
 
 export default ProjectTemplate
