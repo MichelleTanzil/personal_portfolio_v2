51899e351c7501ab58024ca4f1e448496e8609aa
Michelle Tanzil <mtanzil01@gmail.com>
2021-01-13 16:42:38 -0800

    Data on the project template pages

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -2,16 +2,82 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Title from "../components/Title"
+import * as FontAwesome from "react-icons/fa"
+import * as SiIcons from "react-icons/si"
+import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
+
 const ProjectTemplate = ({ data }) => {
   const {
     title: { text: titleText },
+    image: {
+      fluid: { src: image_url },
+    },
+    image: { alt: image_alt },
+    completed_on,
+    status,
+    description: { text: descriptionText },
+    github: { url: githubLink },
+    url: { url: deployedLink },
+    hashtags: { text: hashTags },
+    tech_stack,
   } = data.project.data
+
+  const techStack = tech_stack.map(tech => {
+    return {
+      techName: tech.technology.text,
+      icon: tech.icon.text,
+    }
+  })
   return (
     <Layout>
       <section className="project-template">
         <div className="section-center">
           <article className="project-content">
             <Title title={titleText} />
+            <div>
+              <img
+                src={image_url}
+                className="project-template-img"
+                alt={image_alt}
+              />
+              <p className="project-text">{descriptionText}</p>
+            </div>
+            <div>
+              <div className="project-stack">
+                {techStack.map((tech, index) => {
+                  // Dynamically create the icons
+                  const IconName =
+                    tech.icon[0] === "F"
+                      ? FontAwesome[tech.icon]
+                      : tech.icon[0] === "S"
+                      ? SiIcons[tech.icon]
+                      : "div"
+                  return (
+                    <div key={`Project_${titleText}_tech_${index}`}>
+                      <p className="stacks">
+                        {React.createElement(IconName, { className: "icons" })}
+                        {tech.techName}{" "}
+                      </p>
+                    </div>
+                  )
+                })}
+              </div>
+              <div className="project-links">
+                {githubLink && (
+                  <a href={githubLink} target="_blank">
+                    <FaGithubSquare className="project-icon"></FaGithubSquare>
+                  </a>
+                )}
+                {deployedLink && (
+                  <a href={deployedLink} target="_blank">
+                    <FaShareSquare className="project-icon"></FaShareSquare>
+                  </a>
+                )}
+              </div>
+              <p>Hashtags: {hashTags}</p>
+              <p>Status: {status}</p>
+              {completed_on && <p>Completed on: {completed_on}</p>}
+            </div>
           </article>
         </div>
       </section>
@@ -37,9 +103,6 @@ export const query = graphql`
           fluid {
             src
           }
-        }
-        narrative {
-          text
         }
         status
         title {
