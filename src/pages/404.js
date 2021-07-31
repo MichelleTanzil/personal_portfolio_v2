56c65cdc13af0362173aa89305085f8deaf07bb3
MichelleTanzil <mtanzil01@gmail.com>
2021-07-31 12:40:43 -0700

    404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,18 +2,24 @@ import React from "react"
 import Layout from "../components/Layout"
 import { Link } from "gatsby"
 import SEO from "../components/SEO"
-import { ImArrowLeft } from "react-icons/im"
+import errorImg from "../assets/error-img.svg"
 
 const Error = () => {
   return (
     <Layout isTemplatePage={true}>
       <SEO title="Error page" description="Error page" />
       <main className="error-page">
-        <div className="error-container">
-          <h1>oops it's a dead end</h1>
-          <Link to="/" className="bk-btn">
-            <ImArrowLeft /> Back Home
-          </Link>
+        <div className="section section-center error-container">
+          <img src={errorImg} alt={"Error page"} className="error-img" />
+          <div className="error-info">
+            <h2>oops it's a dead end</h2>
+            <div>
+              <Link to="/" className="error-link">
+                visit homepage
+              </Link>
+              <div className="error-underline"></div>
+            </div>
+          </div>
         </div>
       </main>
     </Layout>
